43199012758655342527251c25592d2b9b652b72
Justin Dietz <JoAMoS.Dev@gmail.com>
2022-10-18 14:20:38 -0700

    Lab 15 & 16
    * adds scope generator
    
    * run generator
    
    * pipeline
    
    * remove tests and update ci
    
    * fix package.json
    
    * set correct trunk branch
    
    * remove test
    
    * again

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,62 +1,7 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  getWorkspaceLayout,
-  installPackagesTask,
-  names,
-  offsetFromRoot,
-  Tree,
-} from '@nrwl/devkit';
+import { formatFiles, installPackagesTask, Tree } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
-
-interface NormalizedSchema extends UtilLibGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
-}
-
-function normalizeOptions(
-  tree: Tree,
-  options: UtilLibGeneratorSchema
-): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
-  };
-}
-
-function addFiles(tree: Tree, options: NormalizedSchema) {
-  const templateOptions = {
-    ...options,
-    ...names(options.name),
-    offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
-  };
-  generateFiles(
-    tree,
-    path.join(__dirname, 'files'),
-    options.projectRoot,
-    templateOptions
-  );
-}
 
 export default async function (tree: Tree, schema: UtilLibGeneratorSchema) {
   await libraryGenerator(tree, {
