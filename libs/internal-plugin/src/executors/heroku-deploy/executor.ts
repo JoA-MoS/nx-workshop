4d3193e4c22feb30ca51f8897f1342b67d131dbc
JoA-MoS <joamos.dev@gmail.com>
2022-10-18 16:31:18 -0700

    set stdio for executor

--- libs/internal-plugin/src/executors/heroku-deploy/executor.ts
+++ libs/internal-plugin/src/executors/heroku-deploy/executor.ts
@@ -3,10 +3,14 @@ import { HerokuDeployExecutorSchema } from './schema';
 
 export default async function runExecutor(schema: HerokuDeployExecutorSchema) {
   const cwd = schema.distLocation;
-  execSync(`heroku container:login`, { cwd });
-  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd });
+  execSync(`heroku container:login`, { cwd, stdio: 'inherit' });
+  execSync(`heroku container:push web --app ${schema.herokuAppName}`, {
+    cwd,
+    stdio: 'inherit',
+  });
   execSync(`heroku container:release web --app ${schema.herokuAppName}`, {
     cwd,
+    stdio: 'inherit',
   });
   return {
     success: true,
